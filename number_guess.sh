0aaf6153da419585e024dd2c30e6a1066e696172
Oktam20 <matejgornal@gmail.com>
2022-04-22 19:28:11 +0000

    feat: Added guessing algorithm

--- number_guess.sh
+++ number_guess.sh
@@ -2,9 +2,31 @@
 
 PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
 
+COUNTER=0
 GAME(){
   echo "Guess the secret number between 1 and 1000:"
   RANDOM_NUM=$(( $RANDOM % 1000 + 1 ))
+  echo $RANDOM_NUM
+  while [[ $GUESS != $RANDOM_NUM ]]
+  do
+    read GUESS
+    if [[ ! $GUESS =~ [0-9]+ ]]
+    then
+      echo "That is not an integer, guess again:"
+    else
+      if [[ $GUESS -gt $RANDOM_NUM ]]
+        then
+          echo "It's lower than that, guess again:"
+      fi
+
+      if [[ $GUESS -lt $RANDOM_NUM ]]
+        then
+          echo "It's higher than that, guess again:"
+      fi
+      let COUNTER++
+    fi
+  done
+  echo "Tries:" $COUNTER
 }
 
 read USERNAME
@@ -21,8 +43,8 @@ then
   fi
 
   echo "Welcome back $USER! You have played $PLAYED games, and your best game took $BEST guesses."
-  GAME
   done
+  GAME
 
 else
   echo "Welcome, $USERNAME! It looks like this is your first time here."
